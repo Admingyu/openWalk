f958d460774707e9bbbaaed6ae6de5d1206d0ed2
Admingyu <gemingyu@126.com>
2021-09-06 23:55:29 +0800

    finish prototype

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,12 @@
 #include <TFT_eSPI.h>
 #include <SPI.h>
 
-
-// A few colour codes:
- 
-// code	color
-// 0x0000	Black
-// 0xFFFF	White
-// 0xBDF7	Light Gray
-// 0x7BEF	Dark Gray
-// 0xF800	Red
-// 0xFFE0	Yellow
-// 0xFBE0	Orange
-// 0x79E0	Brown
-// 0x7E0	Green
-// 0x7FF	Cyan
-// 0x1F	Blue
-// 0xF81F	Pink
-
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
-uint32_t targetTime = 0;       // for next 1 second timeout
+uint8_t targetTime = 10;       // 目标时间 分钟
+uint32_t curTime = 0;        // curTime
+uint32_t lastTime = 0;       // for next 1 second timeout
 
+uint8_t BTN1_PIN = PIN_D1;
 uint8_t SPIN_SENSOR_PIN = PIN_D2;
 uint8_t spinSensorStatus = LOW; 
 uint8_t curSpinSensorStatus = LOW; 
@@ -30,20 +16,58 @@ uint16_t distance = 0;
 float kcl = 0;
 float speed = 0;
 float avgSpeed = 0;
+bool inited = false;
 
 void setup() {
   pinMode(SPIN_SENSOR_PIN, INPUT);
+  pinMode(BTN1_PIN, INPUT);
+  randomSeed(analogRead(PIN_D6));
   
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
   tft.setTextSize(1);
   tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+}
 
-  targetTime = millis() + 1000;
+// 设定运动时长,飞轮转一圈自动开始
+void setTarget(){
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextFont(2);
+  tft.drawString("SET TARGET:",30,0,2);
+  while (digitalRead(SPIN_SENSOR_PIN) == LOW){
+    if (digitalRead(BTN1_PIN) == HIGH) { // 按一下加一分钟
+      byte xpos = 30;
+      byte ypos = 60;
+      targetTime += 1;
+      tft.setTextColor(TFT_CYAN); // Cyan 
+      tft.fillRect(xpos, ypos, tft.width()-30, tft.height()/2, TFT_BLACK);
+      xpos+= tft.drawNumber(targetTime,xpos,ypos,7); // Draw distance
+      tft.drawString(" Min",xpos,38,2); // Draw distance
+    }
+    delay(500);
+  }
+  tft.fillScreen(TFT_BLACK);
+}
+
+// 打印模板
+void initExec(){
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextFont(2);
+  tft.drawString("KCL:",0,64,2); 
+  tft.drawString("POWER:",0,84,2); 
+  // tft.drawString("POWER:",0,104,2); 
+  tft.drawLine(0,60,tft.width(),60,TFT_SKYBLUE);//上下分割线
+  tft.drawRect(0,104,tft.width(), tft.height()-104,TFT_CYAN); //进度条
+  lastTime = millis() + 1000;
+  inited = true;
 }
 
 void loop() {
+  if (!inited){
+    setTarget();
+    initExec();
+  }
 
   // laps count
   curSpinSensorStatus = digitalRead(SPIN_SENSOR_PIN);
@@ -52,19 +76,24 @@ void loop() {
       cirCount++;
       distance += 1;
       kcl += 0.01;
-      speed = 1/(millis()-targetTime);
+      
     }
+    // tft.println(curSpinSensorStatus);
   }
   spinSensorStatus = curSpinSensorStatus;
 
-
-  if (targetTime + 1000 < millis()) {
+  curTime =  millis();
+  if (lastTime + 1000 < curTime) {
     // ============假装每秒更新一次数据==========
     distance += 1;
     kcl += 0.01;
-    speed = 3.2;
+    // speed = 1000.00/(curTime-lastTime);
+    speed = random(50);
     // ============假装每秒更新一次数据==========
-    tft.fillScreen(TFT_BLACK);
+
+    // 屏幕上半区域覆盖黑色
+    tft.fillRect(0,0,tft.width(),59,TFT_BLACK);
+
     
     byte xpos = 6;
     byte ypos = 0;
@@ -74,15 +103,39 @@ void loop() {
 
     tft.setTextColor(TFT_GREEN); // Orange 
     xpos+= tft.drawNumber(distance,xpos,ypos,7); // Draw distance
-    xpos+= tft.drawString("KM",xpos,60,2); // Draw distance
+    xpos+= tft.drawString(" KM",xpos,38,2); // Draw distance
 
     // xpos += tft.drawChar(':', xpos, ypos - 8, 8);
 
-    tft.setTextFont(2);
-    tft.print(kcl);
-    tft.println(" KCL");
-    tft.println("DIST:3080");
+    //里程速度区域覆盖黑色
+    tft.fillRect(50, tft.height()/2, 50, tft.height()-88, TFT_BLACK);
 
-    targetTime = millis()+1000;
+    // 里程速度显示
+    tft.setTextFont(2);
+    tft.drawFloat(kcl,1,50,64); 
+    tft.drawFloat(speed,1,50,84); 
+    // tft.drawFloat(speed,1,50,104);
+
+    // 进度
+    float progress ;
+    progress = (curTime*1.000)/(1000*60*targetTime);
+
+    // 统计图显示
+    tft.drawFastVLine(100 +int(progress*64.00), (114-int(speed)), int(speed), TFT_VIOLET);
+
+    // 进度条显示
+    tft.fillRect(1, 105, tft.width(), tft.height()-106, TFT_BLACK); //进度条黑底
+    tft.fillRect(1, 105, int(tft.width()*progress), tft.height()-106, TFT_NAVY); //进度条
+    tft.setTextColor(TFT_RED); // TFT_OLIVE 
+    
+    tft.drawFloat(progress*100, 1, 50, 110,2); //进度条百分比
+    tft.drawString("%", 80, 110); //进度条百分比
+
+    lastTime = curTime;
+  }
+
+  // 设定初始化完成
+  if (inited ==false){
+    inited=true;
   }
 }
