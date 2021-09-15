71ecdc19e269e17a6c7d9571feea69d03e75837b
Admingyu <gemingyu@126.com>
2021-09-16 00:03:33 +0800

    增加WiFi连接，wifi图标，优化结算页面，增加结算页面图表

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <SPI.h>
 #include <car.h>
 #include <nice.h>
+#include <ESP8266WiFiMulti.h>
+#include <wifi_icon.h>
+
+ESP8266WiFiMulti wifiMulti;
+const uint32_t connectTimeoutMs = 5000;
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 uint8_t targetTime = 10;       // 目标时间 分钟
@@ -20,15 +25,15 @@ float speed = 0;
 float avgSpeed = 0;
 bool inited = false;
 bool finished = false;
-float pStat[256]; 
+float pStat[7200]; 
 
 
 // logo显示
 void dispLogo(){
   tft.fillScreen(TFT_BLACK);
   tft.setTextFont(2);
-  tft.drawCentreString("ZXQ's Fit:", tft.width()/2, tft.height()/2, 2); 
   tft.pushImage(tft.width()/2-32,tft.height()/2-32,64,64, (uint16_t  *)gImage_bik);
+  tft.drawString("ZXQ's Fit:", tft.width()/2, tft.height(), 2); 
 }
 
 void setup() {
@@ -40,7 +45,13 @@ void setup() {
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
   dispLogo();
-  delay(3000);
+  // gImage_wifi_off
+  tft.pushImage(tft.width()-20, 0, 20, 20, (uint16_t  *)gImage_wifi_off);
+  // Register multi WiFi networks
+  wifiMulti.addAP("Satelite", "admingyu123");
+  wifiMulti.addAP("BHU", "admingyu123");
+
+
   tft.setTextSize(1);
   tft.setTextColor(TFT_YELLOW, TFT_BLACK);
 }
@@ -70,11 +81,25 @@ void dispFinish(){
   tft.fillScreen(TFT_BLACK);
   tft.pushImage(0, 0, 64, 64, (uint16_t  *)gImage_nice);
   tft.setTextFont(2);
-  tft.drawString("KCL:",tft.width()/2, 0, 2); 
-  tft.drawString("TIME:",tft.width()/2, 21, 2); 
-  tft.drawString("TRAVEL:",tft.width()/2, 42, 2); 
+  char kcls[10], times[10], dists[10];
+  char kcl_str[6];   //浮点类型转化存储缓冲数组
+  dtostrf(kcl, 4,2, kcl_str);  // 4,2 表示转化的精度
+  sprintf(kcls,"KCL: %s",kcl_str);
+  sprintf(times,"TIME:%dmin", targetTime);
+  sprintf(dists,"DIST: %dm", distance);
+  tft.drawString(kcls ,tft.width()/2, 0, 2); 
+  tft.drawString(times,tft.width()/2, 21, 2); 
+  tft.drawString(dists,tft.width()/2, 42, 2); 
   tft.drawFastHLine(0, tft.height()/2, tft.width(), TFT_SKYBLUE);
-  delay(2000);
+
+  // 统计图
+  unsigned char i;
+  for (i=0; i<7200;i++){
+    tft.drawPixel(i*int(tft.width()/7200), pStat[i]*20, TFT_NAVY);
+  }
+  while(1){
+   delay(2000);
+  }
 }
 
 
@@ -86,13 +111,23 @@ void initExec(){
   tft.drawString("POWER:",0,84,2); 
   // tft.drawString("POWER:",0,104,2); 
   tft.drawLine(0,60,tft.width(),60,TFT_SKYBLUE);//上下分割线
-  tft.drawRect(0,104,tft.width(), tft.height()-104,TFT_CYAN); //进度条
+  tft.drawRect(0,104,tft.width()-1, tft.height()-104,TFT_CYAN); //进度条
   lastTime = millis() + 1000;
   inited = true;
 }
 
 
 void loop() {
+
+  // WIFI连接
+  if (wifiMulti.run(connectTimeoutMs) == WL_CONNECTED) {
+    // gImage_wifi_on
+    tft.pushImage(tft.width()-20, 0, 20, 20, (uint16_t  *)gImage_wifi_on);
+  } else {
+    // gImage_wifi_off
+    tft.pushImage(tft.width()-20, 0, 20, 20, (uint16_t  *)gImage_wifi_off);
+  }
+
   // 是否初始化完成
   if (!inited){
     setTarget();
@@ -103,6 +138,7 @@ void loop() {
   if (finished){
     dispFinish();
     delay(100);
+    return;
   }
 
   // laps count
@@ -121,10 +157,11 @@ void loop() {
   curTime =  millis();
   if (lastTime + 1000 < curTime) {
     // ============假装每秒更新一次数据==========
-    distance += 1;
-    kcl += 0.01;
+    distance += cirCount;
+    kcl += cirCount*0.01;
     // speed = 1000.00/(curTime-lastTime);
-    speed = random(50);
+    speed = cirCount*12;
+    pStat[int(curTime/1000)] = speed;
     // ============假装每秒更新一次数据==========
 
     // 屏幕上半区域覆盖黑色
@@ -139,7 +176,7 @@ void loop() {
 
     tft.setTextColor(TFT_GREEN); // Orange 
     xpos+= tft.drawNumber(distance,xpos,ypos,7); // Draw distance
-    xpos+= tft.drawString(" KM",xpos,38,2); // Draw distance
+    xpos+= tft.drawString(" M",xpos,38,2); // Draw distance
 
     // xpos += tft.drawChar(':', xpos, ypos - 8, 8);
 
@@ -157,7 +194,7 @@ void loop() {
     progress = (curTime*1.000)/(1000*60*targetTime);
 
     // 统计图显示
-    tft.drawFastVLine(100 +int(progress*64.00), (114-int(speed)), int(speed), TFT_VIOLET);
+    tft.drawFastVLine(100 +int(progress*64.00), (114-int(speed)), int(speed), TFT_SKYBLUE);
 
     // 进度条显示
     tft.fillRect(1, 105, tft.width(), tft.height()-106, TFT_BLACK); //进度条黑底
@@ -168,6 +205,7 @@ void loop() {
     tft.drawString("%", 80, 110); //进度条百分比
 
     lastTime = curTime;
+    cirCount = 0;
 
     if (progress >= 1.00){
       finished = true;
